dc6dd3a0070990defcf5f751c149813ff2473357
dameon green <dameongreen@gmail.com>
2023-08-26 14:36:00 -0400

    444

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from "@clerk/nextjs/server";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
-   publicRoutes: ["/", "/cart", "/checkout","/product","/category"],
+   publicRoutes: ["/", "/cart", "/checkout"],
   //  ignoredRoutes: ["/((?!api|trpc))(_next|.+\..+)(.*)", "/product/52bc3532-5dcd-423d-91a8-853279502437"],
 });
 
